2b8a87b467c039ce4307919e4d68f143676f247d
emilnordstroem <emilstoeve@outlook.com>
2025-09-15 16:50:57 +0200

    Photos correctly updates and is added. Issue of updating counter fixed.

--- YatzyProjekt/script/index.js
+++ YatzyProjekt/script/index.js
@@ -1,9 +1,10 @@
 const diceObjects = [
-    {diceNo: 1, diceImg: ".\images\dice-side-one.png"},
-    {diceNo: 2, diceImg: ".\images\dice-side-two.png"},
-    {diceNo: 3, diceImg: ".\images\dice-side-three.png"},
-    {diceNo: 4, diceImg: ".\images\dice-side-four.png"},
-    {diceNo: 5, diceImg: ".\images\dice-side-five.png"}
+    {diceNo: 1, diceImg: "images/dice-side-one.png"},
+    {diceNo: 2, diceImg: "images/dice-side-two.png"},
+    {diceNo: 3, diceImg: "images/dice-side-three.png"},
+    {diceNo: 4, diceImg: "images/dice-side-four.png"},
+    {diceNo: 5, diceImg: "images/dice-side-five.png"},
+    {diceNo: 6, diceImg: "images/dice-side-six.png"}
 ];
 
 const turnLimit = 3;
@@ -13,7 +14,8 @@ function onSumbit () {
     const boardElement = document.getElementById("board");
     boardElement.addEventListener("submit", (event) => {
         event.preventDefault();
-        if(updateTurnCounter()) {
+        if(turnCount <= turnLimit) {
+            updateTurnCounter();
             rollRemainingDices();
             enableRemainingDiceCheckboxes();
         } else {
@@ -26,20 +28,17 @@ function onSumbit () {
 onSumbit();
 
 function updateTurnCounter () {
-    turnCount++;
     const turnCountLabel = document.getElementById("turn-count");
     turnCountLabel.textContent = turnCount;
-    if (turnCount == turnLimit) {
-        return false;
-    }
-    return true;
+    turnCount++;
 }
 
 function rollRemainingDices () {
     const dices = document.querySelectorAll("#dices > input");
     for (const dice of dices) {
         if (!hasDiceBeenChosen(dice)) {
-            
+            const rolledDiceIndex = Math.floor(Math.random() * diceObjects.length);
+            dice.style.backgroundImage = `url(${diceObjects[rolledDiceIndex].diceImg})`;
         }
     }
 }
