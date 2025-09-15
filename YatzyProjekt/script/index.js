b99583d7d471a6162aff875db97532c333cfe123
emilnordstroem <emilstoeve@outlook.com>
2025-09-15 18:46:48 +0200

    Score cards until small straight is implemented

--- YatzyProjekt/script/index.js
+++ YatzyProjekt/script/index.js
@@ -1,3 +1,5 @@
+
+// Upper Section : Dice Roll and Turn overview
 const diceObjects = [
     {diceNo: 1, diceImg: "images/dice-side-one.png"},
     {diceNo: 2, diceImg: "images/dice-side-two.png"},
@@ -7,6 +9,14 @@ const diceObjects = [
     {diceNo: 6, diceImg: "images/dice-side-six.png"}
 ];
 
+const dicesRolled = [
+    {diceNo: 1, count: 0},
+    {diceNo: 2, count: 0},
+    {diceNo: 3, count: 0},
+    {diceNo: 4, count: 0},
+    {diceNo: 5, count: 0},
+    {diceNo: 6, count: 0}
+];
 
 const turnLimit = 3;
 let turnCount = 0;
@@ -16,9 +26,11 @@ function onSumbit () {
     submitElement.addEventListener("click", (event) => {
         event.preventDefault();
         if(turnCount < turnLimit) {
+            resetDicesRolled();
             updateTurnCounterLabel();
             rollRemainingDices();
             enableRemainingDiceCheckboxes();
+            updateScoreCards();
         } else {
             disableAllDiceCheckboxes();
             toggleSubmitButton();
@@ -27,6 +39,12 @@ function onSumbit () {
 }
 
 onSumbit();
+
+function resetDicesRolled () {
+    for (const dice of dicesRolled) {
+        dice.count = 0;
+    }
+}
 
 function updateTurnCounterLabel () {
     turnCount++;
@@ -39,6 +57,7 @@ function rollRemainingDices () {
     for (const dice of dices) {
         if (!hasDiceBeenChosen(dice)) {
             const rolledDiceIndex = Math.floor(Math.random() * diceObjects.length);
+            dicesRolled[rolledDiceIndex].count++;
             dice.style.backgroundImage = `url(${diceObjects[rolledDiceIndex].diceImg})`;
         }
     }
@@ -74,3 +93,128 @@ function toggleSubmitButton () {
         sumbitButton.disabled = false;
     }
 }
+
+// Lower section : Score board and overview
+function updateScoreCards () {
+    const scoreCards = document.querySelectorAll("#dice-score-section input");
+    let initialScore = 0;
+    for (const scoreCard of scoreCards) {
+        switch (scoreCard.id) {
+            case "rolled-ones": 
+                initialScore = howManyPrimitiveWereRolled(0);
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "rolled-twos": 
+                initialScore = howManyPrimitiveWereRolled(1);
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "rolled-threes": 
+                initialScore = howManyPrimitiveWereRolled(2);
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "rolled-fours": 
+                initialScore = howManyPrimitiveWereRolled(3);
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "rolled-fives": 
+                initialScore = howManyPrimitiveWereRolled(4);
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "rolled-sixes": 
+                initialScore = howManyPrimitiveWereRolled(5);
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "one-pair": 
+                initialScore = largestOnePair();
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "two-paires": 
+                initialScore = largestTwoPair();
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "three-same": 
+                initialScore = threeSame();
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "four-same": 
+                initialScore = fourSame();
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            case "full-house": 
+                initialScore = fullHouse();
+                addValueToScoreCard(scoreCard, initialScore);
+                break;
+            default: break;
+        }
+    }
+}
+
+function howManyPrimitiveWereRolled (index) {
+    return dicesRolled[index].count * dicesRolled[index].diceNo;
+}
+
+function largestOnePair () {
+    let pairSum = 0;
+    for (const dice of dicesRolled) {
+        if (dice.count >= 2 && betterPairFound(pairSum, dice)) {
+            pairSum = (2 * dice.diceNo);
+        }
+    }
+    function betterPairFound (previousPairSum, dice) {
+        return previousPairSum < (dice.count * dice.diceNo);
+    }
+    return pairSum;
+}
+
+function largestTwoPair () {
+    let pairCount = 0;
+    let pairSum = 0;
+    for (const dice of dicesRolled) {
+        if (pairCount < 2 && dice.count >= 2 && betterPairFound(pairSum, dice)) {
+            pairCount++;
+            pairSum += (2 * dice.diceNo);
+        }
+    }
+    function betterPairFound (previousPairSum, dice) {
+        return previousPairSum < (dice.count * dice.diceNo);
+    }
+    if (pairCount == 2) {
+        return pairSum;
+    }
+    return 0;
+}
+
+function threeSame () {
+    let sum = 0;
+    for (const dice of dicesRolled) {
+        if (dice.count >= 3) {
+            sum = (3 * dice.diceNo);
+        }
+    }
+    return sum;
+}
+
+function fourSame () {
+    let sum = 0;
+    for (const dice of dicesRolled) {
+        if (dice.count >= 4) {
+            sum = (4 * dice.diceNo);
+        }
+    }
+    return sum;
+}
+
+function fullHouse () {
+    let sum = 0;
+    for (const dice of dicesRolled) {
+        if (dice.count >= 5) {
+            sum = (5 * dice.diceNo);
+        }
+    }
+    return sum;
+}
+
+function addValueToScoreCard (scoreCard, value) {
+    scoreCard.value = value;
+}
+
