220385d8bc0af8727cde46e9dc3b718df25c8bd8
emilnordstroem <emilstoeve@outlook.com>
2025-09-15 15:02:01 +0200

    Turn counter updates upon submit - until turn is 3 or above

--- YatzyProjekt/script/index.js
+++ YatzyProjekt/script/index.js
@@ -1,2 +1,20 @@
+let turnCount = 0;
 
-function 
+function onSumbit () {
+    const boardElement = document.getElementById("board");
+    boardElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        updateTurnCounter();
+    });
+}
+
+onSumbit();
+
+function updateTurnCounter () {
+    if (turnCount >= 3) {
+        return;
+    }
+    const turnCountLabel = document.getElementById("turn-count");
+    turnCount++;
+    turnCountLabel.textContent = turnCount;
+}
