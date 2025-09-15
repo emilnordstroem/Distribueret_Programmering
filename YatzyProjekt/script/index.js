449de8b6e1bbc5c82ec9b817dad5051599d53c36
emilnordstroem <emilstoeve@outlook.com>
2025-09-15 15:05:09 +0200

    Changed updateTurnCounter due to logical error. EnableRemainingDiceCheckBoxes implemented and disabling submit button when limit i reached implemented

--- YatzyProjekt/script/index.js
+++ YatzyProjekt/script/index.js
@@ -1,20 +1,28 @@
+const turnLimit = 3;
 let turnCount = 0;
 
 function onSumbit () {
     const boardElement = document.getElementById("board");
     boardElement.addEventListener("submit", (event) => {
         event.preventDefault();
-        updateTurnCounter();
+        if(updateTurnCounter()) {
+            rollRemainingDices();
+        }
     });
 }
 
 onSumbit();
 
 function updateTurnCounter () {
-    if (turnCount >= 3) {
-        return;
+    if (turnCount >= turnLimit) {
+        return false;
     }
     const turnCountLabel = document.getElementById("turn-count");
     turnCount++;
     turnCountLabel.textContent = turnCount;
+    return true;
+}
+
+function rollRemainingDices () {
+
 }
