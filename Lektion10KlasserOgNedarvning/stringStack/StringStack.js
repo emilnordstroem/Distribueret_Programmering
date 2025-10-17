af6b6a73cf82b3af9c579b4dadf56a7814f65d35
emilnordstroem <emilstoeve@outlook.com>
2025-10-17 11:34:51 +0200

    Completion of task 10.2

---
+++ Lektion10KlasserOgNedarvning/stringStack/StringStack.js
@@ -0,0 +1,27 @@
+
+class StringStack {
+
+    constructor () {
+        this.stackList = []
+        this.length = 0
+    }
+
+    push (string) {
+        this.stackList.push(string);
+        this.length++;
+    }
+
+    pop () {
+        this.length--;
+        return this.stackList.pop();
+    }
+
+}
+
+const stack = new StringStack;
+stack.push("Emil");
+console.log(stack.stackList);
+console.log(`Length: ${stack.length}`);
+console.log(`Pop: ${stack.pop()}`);
+console.log(stack.stackList);
+console.log(`Length: ${stack.length}`);
